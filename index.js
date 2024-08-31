cc14e5026b77c5140c96d8928a2df54afe457d92
unknown <maxsstetler@gmail.com>
2024-08-31 14:36:30 -0400

    finished project

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-new Class = Vehicle;
-
-
 class Vehicle {
-    constructor(a,b,c){
-        this.make = a;
-        this.model = b;
-        this.year = c;
+    constructor(make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
     }
 
     honk(){
@@ -16,4 +13,41 @@ class Vehicle {
         return `The vehicle is a ${this.make} ${this.model} from ${this.year}`;
     }
 
+}
+
+
+class Car extends Vehicle {
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
+
+class Motorcyle extends Vehicle{
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+
+    revEngine(){
+        return "VROOM!!!"
+    }
+}
+
+class Garage {
+    constructor(capacity){
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+
+    add(newVehicle){
+        if (!(newVehicle instanceof Vehicle)){
+            return "Only vehicles are allowed in here!";
+        }
+        if(this.vehicles.length >= this.capacity){
+            return "Sorry, we're full.";
+        }
+        this.vehicles.push(newVehicle);
+        return "Vehicle added!";
+    }
 }
